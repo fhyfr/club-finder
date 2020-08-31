51e064cf09d07302e67896dc71dff87e9498f739
fhyfr <fhaya.firman@gmail.com>
2020-08-31 15:58:24 +0700

    Applied Custom Element

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,9 +1,10 @@
+import '../component/search-bar.js';
+import '../component/club-list.js';
 import DataSource from '../data/data-source.js';
 
 const main =  () => {
-    const searchElement = document.querySelector("#searchElement");
-    const buttonSearchElement = document.querySelector("#searchButtonElement");
-    const clubListElement = document.querySelector("#clubList");
+    const searchElement = document.querySelector("search-bar");
+    const clubListElement = document.querySelector("club-list");
 
     const onButtonSearchClicked = () => {
         DataSource.searchClub(searchElement.value)
@@ -12,29 +13,14 @@ const main =  () => {
     };
 
     const renderResult = results => {
-        clubListElement.innerHTML = "";
-        results.forEach( club => {
-          const {name, fanArt, description} = club;
-
-            const clubElement = document.createElement("div");
-            clubElement.setAttribute("class", "club");
-
-            clubElement.innerHTML = `
-                <img class="fan-art-club" src=" ${fanArt} " alt="Fan Art">\n
-                <div class="club-info">\n
-                <h2> ${name} </h2>\n
-                <p> ${description} </p>
-                </div>`;
-            clubListElement.appendChild(clubElement);
-        })
+        clubListElement.clubs = results;
     };
 
     const fallbackResult =  message => {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += `<h2 class="placeholder"> ${message} </h2>`
+        clubListElement.renderError(message);
     };
 
-    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+    searchElement.clickEvent = onButtonSearchClicked;
 };
 
 export default main;
